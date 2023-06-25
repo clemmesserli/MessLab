a6806ecdb55878eaeb2cfdaa111124dcfafd47a0
Clem Messerli <clemmesserli@gmail.com>
2023-06-24 20:13:20 -0500

    init files

---
+++ LinuxLab01.ps1
@@ -0,0 +1,42 @@
+﻿New-LabDefinition -Name 'LNXLAB01' -VmPath 'C:\LabVMs' -DefaultVirtualizationEngine HyperV
+Add-LabVirtualNetworkDefinition -Name 'External' -HyperVProperties @{SwitchType='External';AdapterName='Wi-Fi'}
+Add-LabMachineDefinition -Name 'LNXLAB01' -Memory '8GB' -Network 'External' -OperatingSystem 'Ubuntu 22.04.2 LTS "Jammy Jellyfish"'
+Install-Lab
+Show-LabDeploymentSummary -Detailed
+
+# Need to manually launch machine via Hyper-V to complete installer for now
+# Once base image is installed and machine restarted, continue with the following
+
+<#
+sudo apt update
+sudo apt install ssh
+sudo apt install git
+sudo apt install ansible-core
+
+ssh-keygen.exe -t ed25519 -C "labadmin"
+# accept defaults and set your password
+
+ls ~/.ssh
+cat id_ed25519.pub  # copy this to Github SSH Keys to allow ansible-pull to authenticate
+
+
+git config --global user.email "clemmesserli@gmail.com"
+git config --global user.name "clemmesserli"
+
+mkdir ~/github
+cd ~/github
+git clone git@github.com:clemmesserli/ansible_desktop.git
+
+nano README.md  # make a simple change
+
+git status # should now indicate a change has been made
+
+git commit -a -m 'Updated README with URI'
+
+git push origin main
+
+
+sudo ansible-pull -i localhost,lnxlab01 -U https://github.com/clemmesserli/ansible_desktop.git
+#>
+
+Enter-PSSession -HostName LNXLAB01 -UserName 'LabAdmin' -Password 'P@ssword1'
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# MessBuild
-Scripts and Configuration files intended to help newcomers get started
+# MessLabs
+
+This repo is intended to help newcomers harness the power of [AutomatedLab] (<https://github.com/AutomatedLab/AutomatedLab>)
+
+## Scripts
+
+## Packages
+
+## Configuration Files
+
+1. gitconfig : Sample showing how to preconfigure git
+2. gitignore : Sample showing how to keep certain files or folders from your PowerShell repository
+3. settings.json : Sample showing how to preconfigure vscode
+4. profile.ps1 : Sample showing how to increase PowerShell productivity
