af03ec682522f53450d75a81a1ae1260f7461454
Clem Messerli <clemmesserli@gmail.com>
2023-06-26 19:49:40 -0500

    edit: add clearhistory function

--- data/powershell/profile.ps1
+++ data/powershell/profile.ps1
@@ -20,15 +20,65 @@ $CmdDate = Get-Date
 Import-Module 'C:\Github\MessKit\MessKit.psm1' -Force
 #endregion
 
+#region Set Custom Aliases
+Set-Alias -Name gms -Value Get-MySecret -Description "Copy MyVault Admin Pwd to ClipBoard"
+Set-Alias -Name ums -Value Unlock-MySecret -Description "Unlock MyVault using encrypted CmsMessage file"
+#endregion
+
 #region Functions
+function Clear-SavedHistory {
+    # src: https://stackoverflow.com/a/38807689
+    [CmdletBinding(ConfirmImpact = 'High', SupportsShouldProcess)]
+    param()
+    $PSReadline = ( $null -ne $(Get-Module PSReadline -ea SilentlyContinue) )
+    $target = if ( $PSReadline ) { "entire command history, including from previous sessions" } else { "command history" }
+    if ( -not $pscmdlet.ShouldProcess($target) ) { return }
+    if ( $PSReadline ) {
+        Clear-Host
+        # Remove PSReadline's saved-history file.
+        if ( Test-Path (Get-PSReadlineOption).HistorySavePath ) {
+            # Abort, if the file for some reason cannot be removed.
+            Remove-Item -ea Stop (Get-PSReadlineOption).HistorySavePath
+            # To be safe, we recreate the file (empty).
+            $null = New-Item -Type File -Path (Get-PSReadlineOption).HistorySavePath
+        }
+        # Clear PowerShell's own history
+        Clear-History
+        [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
+    } else {
+        # Without PSReadline, we only have a *session* history.
+        Clear-Host
+        Clear-History
+    }
+}
+Function Get-MySecret {
+    param(
+        [String]$Name = "MyAdminCred"
+    )
+    Unlock-MySecret
+	(Get-Secret $Name).getnetworkcredential().password.Trim() | Set-Clipboard
+}
 Function Hosts: { Set-Location 'C:\windows\system32\drivers\etc\hosts' }
 Function MessKit: { Set-Location 'C:\GitHub\MessKit' }
 Function MessLab: { Set-Location 'C:\GitHub\MessLab' }
+Function Unlock-MySecret {
+    Unlock-SecretStore -Password (ConvertTo-SecureString -AsPlainText -Force (Unprotect-CmsMessage -Path 'C:\mygithub\MyVault\Data\MyVault.cms')) -PasswordTimeout 28800
+}
+# Drive shortcuts
+function HKLM:  { Set-Location HKLM: }
+function HKCU:  { Set-Location HKCU: }
+function Env:   { Set-Location Env: }
 #endregion
 
 #region IsAdmin
 ## Find out if the current user identity is elevated (has admin rights)
 $IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+## Set PSSession variables for use when running under normal user creds
+if (-not($IsAdmin)) {
+    Unlock-MySecret
+    $MyAdminCred = (Get-Secret -Name 'MyAdminCred' -Vault 'MyVault')
+    $MyUserCred = (Get-Secret -Name 'MyUserCred' -Vault 'MyVault')
+}
 #endregion
 
 #region Prompt
@@ -48,14 +98,14 @@ Function Set-CustomDirectory {
 
     Foreach ($Item in $Path) {
         $Match = ($CustomDirectories.GetEnumerator().name |
-                Where-Object { $Item -eq "$_" -or $Item -like "$_*" } |
-                    Select-Object @{n = 'Directory'; e = { $_ } }, @{n = 'Length'; e = { $_.length } } |
-                        Sort-Object Length -Descending |
-                            Select-Object -First 1).directory
+            Where-Object { $Item -eq "$_" -or $Item -like "$_*" } |
+            Select-Object @{n = 'Directory'; e = { $_ } }, @{n = 'Length'; e = { $_.length } } |
+            Sort-Object Length -Descending |
+            Select-Object -First 1).directory
 
         If ($Match) {
             [String]($Item -replace [regex]::Escape($Match), $CustomDirectories[$Match])
-        } ElseIf ($PWD.Pathh -ne $Item) {
+        } ElseIf ($PWD.Path -ne $Item) {
             $Item
         } Else {
             $PWD.Path
@@ -91,5 +141,12 @@ Function Prompt {
     }
     Write-Host "[$($CmdPromptUser.Name.tolower())] [$(Set-CustomDirectory)] " -ForegroundColor $fgColor -NoNewline
     Write-Host "[$(Get-Date $CmdDate -Format 'dddd hh:mm:ss tt')]" -ForegroundColor $fgColor
+    # Write PS> for desktop PowerShell, pwsh> for PowerShell Core
+    if ($isDesktop) {
+        Write-Host "ps>" -NoNewLine -ForegroundColor $fgColor
+    } else {
+        Write-Host "pwsh>" -NoNewLine -ForegroundColor $fgColor
+    }
+    return " "
 }
 #endregion
