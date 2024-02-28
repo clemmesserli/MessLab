168539f1783ace435adca55f7d7841ca9de2afd9
Clem Messerli <clemmesserli@gmail.com>
2024-02-27 23:33:51 -0600

    edit: add check for use of external lab data storage drive

--- labs/Pre-Requisites.ps1
+++ labs/Pre-Requisites.ps1
@@ -5,14 +5,50 @@ Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Repository PSGallery -Name AutomatedLab -Scope AllUsers -Force -AllowClobber
 
-# Download AutomatedLab resources from Github
-New-LabSourcesFolder
+# Download AutomatedLab LabResources from their Github
+## I use on Laptop (C:) and NUC (L:) so needed to first check for
+$LabDrive = Get-DriveInfo | Where-Object VolumeName -Match 'Lab Data'
+if ($LabDrive) {
+	New-LabSourcesFolder -DriveLetter $LabDrive.DeviceID
+} else {
+	New-LabSourcesFolder
+}
 
 # Copy desired ISO files to
-Copy-Item "$($env:onedrive)\LabSources\ISOs" "C:\Labsources\ISOs"
+Copy-Item "$($env:onedrive)\LabSources\ISOs" "$(Get-LabSourcesLocation)\ISOs"
 
 # Check to see what is available
 $OSOptions = (Get-LabAvailableOperatingSystem).OperatingSystemName
 $OSOptions
 
+# Check to see what your Network Adapter Names are
+# Get-NetAdapter
 
+
+#region Available OS List
+<#
+Kali Linux 2023.4
+Red Hat Enterprise Linux 8.9
+Red Hat Enterprise Linux 9.3
+Ubuntu 22.04.4 LTS "Jammy Jellyfish"
+Ubuntu-Server 22.04.4 LTS "Jammy Jellyfish"
+Windows 10 Enterprise Evaluation
+Windows 11 Enterprise Evaluation
+Windows Server 2012 R2 Standard Evaluation (Server Core Installation)
+Windows Server 2012 R2 Standard Evaluation (Server with a GUI)
+Windows Server 2012 R2 Datacenter Evaluation (Server Core Installation)
+Windows Server 2012 R2 Datacenter Evaluation (Server with a GUI)
+Windows Server 2016 Standard Evaluation
+Windows Server 2016 Standard Evaluation (Desktop Experience)
+Windows Server 2016 Datacenter Evaluation
+Windows Server 2016 Datacenter Evaluation (Desktop Experience)
+Windows Server 2019 Standard Evaluation
+Windows Server 2019 Standard Evaluation (Desktop Experience)
+Windows Server 2019 Datacenter Evaluation
+Windows Server 2019 Datacenter Evaluation (Desktop Experience)
+Windows Server 2022 Standard Evaluation
+Windows Server 2022 Standard Evaluation (Desktop Experience)
+Windows Server 2022 Datacenter Evaluation
+Windows Server 2022 Datacenter Evaluation (Desktop Experience)
+#>
+#endregion
