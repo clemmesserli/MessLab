418a9c04f684701f2b179151a2a395b1f208b48e
Clem Messerli <clemmesserli@gmail.com>
2024-03-07 18:22:21 -0600

    added scripts to pull down dependency files

--- labs/Pre-Requisites.ps1
+++ labs/Pre-Requisites.ps1
@@ -3,10 +3,12 @@ Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
 
 # Install the AutomatedLab module
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+# Don't be alarmed by the module below also installing a number of additional modules which is depends upon
 Install-Module -Repository PSGallery -Name AutomatedLab -Scope AllUsers -Force -AllowClobber
 
 # Download AutomatedLab LabResources from their Github
-## I use on Laptop (C:) and NUC (L:) so needed to first check for
+## C:\LabSources is the default location but I wanted to leverage an external SSD drive for greater storage
 $LabDrive = Get-DriveInfo | Where-Object VolumeName -Match 'Lab Data'
 if ($LabDrive) {
 	New-LabSourcesFolder -DriveLetter $LabDrive.DeviceID
@@ -14,51 +16,63 @@ if ($LabDrive) {
 	New-LabSourcesFolder
 }
 
-# Copy desired ISO files to
-Copy-Item "$($env:onedrive)\LabSources\ISOs" "$(Get-LabSourcesLocation)\ISOs"
+#region Copy ISO files to LabSources\ISOs
+## Due to size, I have copies stored on a fileshare, however, you can download fresh eval copies
+## from : https://www.microsoft.com/en-us/evalcenter
 
+$srcPath = "$($env:onedrive)\LabSources\ISOs"
+$dstPath = "$(Get-LabSourcesLocation)\ISOs"
 
-#region Update SoftwarePackages
-$progressPreference = 'silentlyContinue'
-$latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
-$latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
-Write-Information "Downloading winget to artifacts directory..."
-Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "$(Get-LabSourcesLocation)\SoftwarePackages\$latestWingetMsixBundle"
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile "$(Get-LabSourcesLocation)\SoftwarePackages\Microsoft.VCLibs.x64.14.00.Desktop.appx"
+if (Test-Path -Path $dstPath) {
+	# Compare the files
+	$srcFile = Get-FileHash -Path $srcPath
+	$dstFile = Get-FileHash -Path $dstPath
+
+	if ($srcFile.Hash -ne $dstFile.Hash) {
+		# If the files are not identical, copy the source file to the destination
+		Copy-Item -Path $srcPath -Destination $dstPath
+	}
+} else {
+	# If the destination file does not exist, copy the source file to the destination
+	Copy-Item -Path $srcPath -Destination $dstPath
+}
+#endregion
+
+#region Update LabSources\SoftwarePackages
+$params = @{
+	uri     = 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+	outfile = "$(Get-LabSourcesLocation)\SoftwarePackages\Microsoft.VCLibs.Desktop.appx"
+}
+Invoke-WebRequest @params
+
+$releases = Invoke-RestMethod "https://api.github.com/repos/microsoft/microsoft-ui-xaml/releases"
+$asset = ($releases | Where-Object name -Match 'Microsoft.UI.Xaml')[0].assets | Where-Object name -Match 'x64.appx'
+$params = @{
+	uri     = "$($asset.browser_download_url)"
+	outfile = "$(Get-LabSourcesLocation)\SoftwarePackages\Microsoft.UI.Xaml.appx"
+}
+Invoke-WebRequest @params
+
+$releases = Invoke-RestMethod "https://api.github.com/repos/microsoft/winget-cli/releases"
+$asset = ($releases | Where-Object name -Match 'Windows Package Manager')[0].assets | Where-Object name -Match ".msixbundle$"
+$params = @{
+	uri     = "$($asset.browser_download_url)"
+	outfile = "$(Get-LabSourcesLocation)\SoftwarePackages\winget.msixbundle"
+}
+Invoke-WebRequest @params
+
+$releases = Invoke-RestMethod "https://api.github.com/repos/microsoft/terminal/releases"
+$asset = ($releases | Where-Object name -NotMatch 'preview')[0].assets | Where-Object name -Match ".msixbundle$"
+$params = @{
+	uri     = "$($asset.browser_download_url)"
+	outfile = "$(Get-LabSourcesLocation)\SoftwarePackages\Microsoft.WindowsTerminal.msixbundle"
+}
+Invoke-WebRequest @params
 #endregion
 
 # Check to see what is available
-$OSOptions = (Get-LabAvailableOperatingSystem).OperatingSystemName
-$OSOptions
+# $OSOptions = (Get-LabAvailableOperatingSystem).OperatingSystemName
+# $OSOptions
 
 # Check to see what your Network Adapter Names are
 # Get-NetAdapter
-
-
-#region Available OS List
-<#
-Kali Linux 2023.4
-Red Hat Enterprise Linux 8.9
-Red Hat Enterprise Linux 9.3
-Ubuntu 22.04.4 LTS "Jammy Jellyfish"
-Ubuntu-Server 22.04.4 LTS "Jammy Jellyfish"
-Windows 10 Enterprise Evaluation
-Windows 11 Enterprise Evaluation
-Windows Server 2012 R2 Standard Evaluation (Server Core Installation)
-Windows Server 2012 R2 Standard Evaluation (Server with a GUI)
-Windows Server 2012 R2 Datacenter Evaluation (Server Core Installation)
-Windows Server 2012 R2 Datacenter Evaluation (Server with a GUI)
-Windows Server 2016 Standard Evaluation
-Windows Server 2016 Standard Evaluation (Desktop Experience)
-Windows Server 2016 Datacenter Evaluation
-Windows Server 2016 Datacenter Evaluation (Desktop Experience)
-Windows Server 2019 Standard Evaluation
-Windows Server 2019 Standard Evaluation (Desktop Experience)
-Windows Server 2019 Datacenter Evaluation
-Windows Server 2019 Datacenter Evaluation (Desktop Experience)
-Windows Server 2022 Standard Evaluation
-Windows Server 2022 Standard Evaluation (Desktop Experience)
-Windows Server 2022 Datacenter Evaluation
-Windows Server 2022 Datacenter Evaluation (Desktop Experience)
-#>
-#endregion
